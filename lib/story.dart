b183520ee5476f12c9c4506bb04bdc9313d93046
Zachariah Farhang <69781394+Zachariah-Farhang@users.noreply.github.com>
2023-06-09 07:59:06 +0430

    Step 7 - Uncomment the lines below to include storyData as a private property in StoryBrain. Hint: You might need to change something in story.dart to make this work.

--- lib/story.dart
+++ lib/story.dart
@@ -1,9 +1,7 @@
 class Story {
-  Story(this.storyTitle, this.choice1, this.choice2);
-
   late String storyTitle;
   late String choice1;
   late String choice2;
-}
 
-//TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
+  Story({required this.storyTitle, required this.choice1, required this.choice2});
+}
