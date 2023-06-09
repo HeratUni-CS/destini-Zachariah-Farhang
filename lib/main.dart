ed2a34f63b42a8262d52cb66f08fa26c7f64191a
Zachariah Farhang <69781394+Zachariah-Farhang@users.noreply.github.com>
2023-06-09 08:06:55 +0430

    Step 9 - Create a new storyBrain object from the StoryBrain class.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:destini/story_brain.dart';
 import 'package:flutter/material.dart';
 
 //TODO: Step 15 - Run the app and see if you can see the screen update with the first story. Delete this TODO if it looks as you expected.
@@ -20,6 +21,8 @@ class StoryPage extends StatefulWidget {
 }
 
 class _StoryPageState extends State<StoryPage> {
+  StoryBrain storyBrain = StoryBrain();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
