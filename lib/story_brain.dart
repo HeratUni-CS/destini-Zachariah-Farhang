3e56add55fe8883881ec14485da15eada82992f4
Zachariah Farhang <69781394+Zachariah-Farhang@users.noreply.github.com>
2023-06-09 09:53:37 +0430

    updated nextStory method

--- lib/story_brain.dart
+++ lib/story_brain.dart
@@ -55,14 +55,22 @@ class StoryBrain {
   void nextStory(choiceNumber) {
     if (choiceNumber == 1 && storyNumber == 0) {
       storyNumber = 2;
-    } else if (storyNumber >= 3 && storyNumber <= 5) {
+    } else if (choiceNumber == 2 && storyNumber == 0) {
+      storyNumber = 1;
+    } else if (choiceNumber == 1 && storyNumber == 1) {
+      storyNumber = 2;
+    } else if (choiceNumber == 2 && storyNumber == 1) {
+      storyNumber = 3;
+    } else if (choiceNumber == 1 && storyNumber == 2) {
+      storyNumber = 5;
+    } else if (choiceNumber == 2 && storyNumber == 2) {
+      storyNumber = 4;
+    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
       restart();
     }
   }
 }
 
-
-//TODO: Step 23 - Use the storyNumber property inside getStory(), getChoice1() and getChoice2() so that it gets the updated story and choices rather than always just the first (0th) one.
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
 
